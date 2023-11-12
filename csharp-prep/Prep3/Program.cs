a2f8018999c7f10a16b2ff515f2eea1411e86897
erickpati10 <pat21018@byui.edu>
2023-11-11 23:00:12 -0700

    homework done

--- csharp-prep/Prep3/Program.cs
+++ csharp-prep/Prep3/Program.cs
@@ -3,7 +3,51 @@ using System;
 class Program
 {
     static void Main(string[] args)
-    {
-        Console.WriteLine("Hello Prep3 World!");
+     {
+
+        // Console.Write("What is the magic number? ");
+        // int magicNumber = int.Parse(Console.ReadLine());
+
+        // Console.Write("What is your guess? ");
+        // int userGuess = int.Parse(Console.ReadLine());
+
+        // if (userGuess < magicNumber)
+        // {
+        //     Console.WriteLine("Higher");
+        // }
+        // else if (userGuess > magicNumber)
+        // {
+        //     Console.WriteLine("Lower");
+        // }
+        // else
+        // {
+        //     Console.WriteLine("You guessed it!");
+        // }
+        
+        // Part #3
+
+        Random random = new Random();
+        int magicNumber = random.Next(1, 101); 
+
+        int userGuess;
+
+        do
+        {
+            Console.Write("What is your guess? ");
+            userGuess = Convert.ToInt32(Console.ReadLine());
+
+            if (userGuess < magicNumber)
+            {
+                Console.WriteLine("Higher");
+            }
+            else if (userGuess > magicNumber)
+            {
+                Console.WriteLine("Lower");
+            }
+            else
+            {
+                Console.WriteLine("You guessed it!");
+            }
+        } while (userGuess != magicNumber);
     }
 }
--- csharp-prep/Prep4/Program.cs
+++ csharp-prep/Prep4/Program.cs
@@ -4,6 +4,45 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep4 World!");
+
+        List<int> numbers = new List<int>();
+
+        Console.WriteLine("Enter a list of numbers, type 0 when finished.");
+
+        int number;
+        do
+        {
+            Console.Write("Enter number: ");
+            number = int.Parse(Console.ReadLine());
+
+            if (number != 0)
+            {
+                numbers.Add(number);
+            }
+
+        } while (number != 0);
+
+       
+        Console.WriteLine($"The sum is: {numbers.Sum()}");
+        Console.WriteLine($"The average is: {numbers.Average()}");
+        Console.WriteLine($"The largest number is: {numbers.Max()}");
+
+        // Stretch Challenge
+        List<int> positiveNumbers = numbers.Where(n => n > 0).ToList();
+        int smallestPositiveNumber = positiveNumbers.Count > 0 ? positiveNumbers.Min() : 0;
+
+        Console.WriteLine($"The smallest positive number is: {smallestPositiveNumber}");
+
+        Console.WriteLine("The sorted list is:");
+        numbers.Sort();
+        foreach (int num in numbers)
+        {
+            Console.WriteLine(num);
+        }
     }
 }
+
+
+
+
+
