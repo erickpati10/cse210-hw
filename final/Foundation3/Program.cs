80d9fe485fe48a7cac5fb63705fc9a84891a7f7b
erickpati10 <pat21018@byui.edu>
2023-12-12 17:17:20 -0700

    finish code

--- final/Foundation3/Program.cs
+++ final/Foundation3/Program.cs
@@ -2,8 +2,107 @@ using System;
 
 class Program
 {
-    static void Main(string[] args)
+    static void Main()
     {
-        Console.WriteLine("Hello Foundation3 World!");
+        Vehicle car = new Car("Toyota", "Corolla", 2022, 4, "N");
+        Vehicle car1 = new Car("Infiniti", "Q60", 2023, 2, "Y");
+        Vehicle motorcycle = new Motorcycle("Honda", "CBR", 2021, 1000, "Y");
+        Vehicle truck = new Truck("Volvo", "FH16", 2019, 20, "Y");
+
+        DisplayVehicleDetails(car);
+        DisplayVehicleDetails(car1);
+        DisplayVehicleDetails(motorcycle);
+        DisplayVehicleDetails(truck);
+    }
+
+    static void DisplayVehicleDetails(Vehicle vehicle)
+    {
+        Console.WriteLine("Vehicle Details:");
+        vehicle.DisplayDetails();
+        Console.WriteLine();
     }
 }
+
+
+class Vehicle
+{
+    protected string make;
+    protected string model;
+    protected int year;
+    protected string vehicleType;
+
+    public Vehicle(string make, string model, int year, string vehicleType)
+    {
+        this.make = make;
+        this.model = model;
+        this.year = year;
+        this.vehicleType = vehicleType;
+    }
+
+    public virtual void DisplayDetails()
+    {
+        Console.WriteLine($"Type: {vehicleType}");
+        Console.WriteLine($"Make: {make}");
+        Console.WriteLine($"Model: {model}");
+        Console.WriteLine($"Year: {year}");
+    }
+}
+
+class Car : Vehicle
+{
+    private int numDoors;
+    private string isConvertible;
+
+    public Car(string make, string model, int year, int numDoors, string isConvertible)
+        : base(make, model, year, "Car")
+    {
+        this.numDoors = numDoors;
+        this.isConvertible = isConvertible;
+    }
+
+    public override void DisplayDetails()
+    {
+        base.DisplayDetails();
+        Console.WriteLine($"Number of Doors: {numDoors} \nConvertible: {isConvertible}");
+    }
+}
+
+class Motorcycle : Vehicle
+{
+    private int engineCapacity;
+    private string hasFairing;
+
+    public Motorcycle(string make, string model, int year, int engineCapacity, string hasFairing)
+        : base(make, model, year, "Motorcycle")
+    {
+        this.engineCapacity = engineCapacity;
+        this.hasFairing = hasFairing;
+    }
+
+    public override void DisplayDetails()
+    {
+        base.DisplayDetails();
+        Console.WriteLine($"Engine Capacity: {engineCapacity}cc \nFairing: {hasFairing}");
+    }
+}
+
+class Truck : Vehicle
+{
+    private int cargoCapacity;
+    private string hasSleeperCab;
+
+    public Truck(string make, string model, int year, int cargoCapacity, string hasSleeperCab)
+        : base(make, model, year, "Truck")
+    {
+        this.cargoCapacity = cargoCapacity;
+        this.hasSleeperCab = hasSleeperCab;
+    }
+
+    public override void DisplayDetails()
+    {
+        base.DisplayDetails();
+        Console.WriteLine($"Cargo Capacity: {cargoCapacity} tons \nSleeper Cab: {hasSleeperCab}");
+    }
+}
+
+
--- final/Foundation4/Program.cs
+++ final/Foundation4/Program.cs
@@ -2,8 +2,99 @@ using System;
 
 class Program
 {
-    static void Main(string[] args)
+    static void Main()
     {
-        Console.WriteLine("Hello Foundation4 World!");
+        BankAccount savingsAccount = new SavingsAccount("SAV-123456");
+        BankAccount checkingAccount = new CheckingAccount("CHEK-789012");
+
+        savingsAccount.Deposit(500);
+        checkingAccount.Deposit(1000);
+
+        savingsAccount.Withdraw(100);
+        checkingAccount.Withdraw(200);
+
+        DisplayAccountDetails(savingsAccount);
+        DisplayAccountDetails(checkingAccount);
+    }
+
+    static void DisplayAccountDetails(BankAccount account)
+    {
+        Console.WriteLine("Hi Erick!");
+        account.DisplayBalance();
+        Console.WriteLine();
     }
 }
+class BankAccount
+{
+    protected string accountNumber;
+    protected decimal balance;
+
+    public BankAccount(string accountNumber)
+    {
+        this.accountNumber = accountNumber;
+        balance = 0;
+    }
+
+    public virtual void Deposit(decimal amount)
+    {
+        balance += amount;
+    }
+
+    public virtual void Withdraw(decimal amount)
+    {
+        if (amount <= balance)
+        {
+            balance -= amount;
+        }
+        else
+        {
+            Console.WriteLine("Insufficient funds.");
+        }
+    }
+
+    public virtual void DisplayBalance()
+    {
+        Console.WriteLine($"Account Number: {accountNumber} \nBalance: ${balance}");
+        Console.WriteLine("Account Type: Undefined");
+    }
+}
+
+class SavingsAccount : BankAccount
+{
+    public SavingsAccount(string accountNumber)
+        : base(accountNumber)
+    {
+    }
+
+    public override void Withdraw(decimal amount)
+    {
+        base.Withdraw(amount);
+    }
+
+    public override void DisplayBalance()
+    {
+        Console.WriteLine($"Account Number: {accountNumber} \nBalance: ${balance}");
+        Console.WriteLine("Account: Savings Account");
+    }
+}
+
+class CheckingAccount : BankAccount
+{
+    public CheckingAccount(string accountNumber)
+        : base(accountNumber)
+    {
+    }
+
+    public override void Withdraw(decimal amount)
+    {
+        base.Withdraw(amount);
+    }
+
+    public override void DisplayBalance()
+    {
+        Console.WriteLine($"Account Number: {accountNumber} \nBalance: ${balance}");
+        Console.WriteLine("Account: Checking Account");
+    }
+}
+
+
